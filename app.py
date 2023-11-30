e64e9458b95010a9f49c7f97954ad6df655a7838
Akash Biswas <akash06032002@gmail.com>
2023-11-30 13:26:25 +0530

    Sklearn module error resolved

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_restful import Resource, Api
 import pickle
-import pandas as pd
 from flask_cors import CORS
-from flask import redirect
 
 def create_app():
     app = Flask(__name__)
@@ -16,8 +14,9 @@ def create_app():
 
     class MentalHealthPrediction(Resource):
         def get(self, steps=None, heart_rate=None, sleep_duration=None, stress=None):
+            # Check if parameters are provided in the URL path
             if all(param is not None for param in [steps, heart_rate, sleep_duration, stress]):
-                # Path parameters provided, proceed with prediction
+                # Parameters provided in the URL path, proceed with prediction
                 try:
                     # Convert the values to the appropriate data types
                     steps = int(steps)
@@ -25,27 +24,43 @@ def create_app():
                     sleep_duration = float(sleep_duration)
                     stress = int(stress)
 
-                    # Add your model loading and prediction logic here
-                    # For demonstration purposes, we'll use a placeholder model
-                    # Replace the following lines with your actual model loading and prediction code
-                    model = pickle.load(open('model.pkl', 'rb'))
-                    features = [[1, steps, heart_rate, sleep_duration, stress]]
-                    prediction = model.predict(features)
+                except ValueError as e:
+                    return {"error": f"Invalid value provided: {e}"}, 400
 
-                    # Return the prediction as JSON
-                    return {"prediction": int(prediction[0])}
-
-                except Exception as e:
-                    # Handle exceptions, such as invalid input
-                    return {"error": str(e)}, 400  # Return a 400 status code for bad requests
+            # If not all parameters are provided in the URL path, check query parameters
             else:
-                # No specific path parameters provided
-                return {"message": "Please provide values for Steps, HeartRate, SleepDuration, and Stress in the URL."}, 400
+                # Access all query parameters as a dictionary
+                params = request.args.to_dict()
+
+                if all(param in params for param in ['steps', 'heart_rate', 'sleep_duration', 'stress']):
+                    # Parameters provided in the query parameters, proceed with prediction
+                    try:
+                        # Convert the values to the appropriate data types
+                        steps = int(params['steps'])
+                        heart_rate = int(params['heart_rate'])
+                        sleep_duration = float(params['sleep_duration'])
+                        stress = int(params['stress'])
+
+                    except ValueError as e:
+                        return {"error": f"Invalid value provided: {e}"}, 400
+
+                else:
+                    # Not all required parameters provided
+                    return {"message": "Please provide values for Steps, HeartRate, SleepDuration, and Stress."}, 400
+
+            # Add your model loading and prediction logic here
+            # For demonstration purposes, we'll use a placeholder model
+            # Replace the following lines with your actual model loading and prediction code
+            model = pickle.load(open('model.pkl', 'rb'))
+            features = [[1, steps, heart_rate, sleep_duration, stress]]
+            prediction = model.predict(features)
+
+            # Return the prediction as JSON
+            return {"prediction": int(prediction[0])}
 
     api.add_resource(MentalHealthPrediction, '/predict', '/predict/<int:steps>/<int:heart_rate>/<float:sleep_duration>/<int:stress>')
-
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)

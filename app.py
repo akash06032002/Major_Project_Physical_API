1567fd0537db93105ec28aa5459cf946c3f1aba7
Akash Biswas <akash06032002@gmail.com>
2023-11-30 11:45:52 +0530

    Api ready for deploymeny

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ api.add_resource(MentalHealthPrediction, '/predict', '/predict/<int:steps>/<int:
 
 if __name__ == '__main__':
     # Run the app on a specific port (e.g., 5000)
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)

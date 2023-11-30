ccf236be2b07559e92967f2e83335151d7af912c
Akash Biswas <akash06032002@gmail.com>
2023-11-30 12:29:48 +0530

    Added runtime.txt

--- app.py
+++ app.py
@@ -3,41 +3,49 @@ from flask_restful import Resource, Api
 import pickle
 import pandas as pd
 from flask_cors import CORS
+from flask import redirect
 
-app = Flask(__name__)
-CORS(app)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    api = Api(app)
 
-class MentalHealthPrediction(Resource):
-    def get(self, steps=None, heart_rate=None, sleep_duration=None, stress=None):
-        if all(param is not None for param in [steps, heart_rate, sleep_duration, stress]):
-            # Path parameters provided, proceed with prediction
-            try:
-                # Convert the values to the appropriate data types
-                steps = int(steps)
-                heart_rate = int(heart_rate)
-                sleep_duration = float(sleep_duration)
-                stress = int(stress)
+    @app.route('/')
+    def home():
+        return redirect('/predict')
 
-                # Add your model loading and prediction logic here
-                # For demonstration purposes, we'll use a placeholder model
-                # Replace the following lines with your actual model loading and prediction code
-                model = pickle.load(open('model.pkl', 'rb'))
-                features = [[1,steps, heart_rate, sleep_duration, stress]]
-                prediction = model.predict(features)
+    class MentalHealthPrediction(Resource):
+        def get(self, steps=None, heart_rate=None, sleep_duration=None, stress=None):
+            if all(param is not None for param in [steps, heart_rate, sleep_duration, stress]):
+                # Path parameters provided, proceed with prediction
+                try:
+                    # Convert the values to the appropriate data types
+                    steps = int(steps)
+                    heart_rate = int(heart_rate)
+                    sleep_duration = float(sleep_duration)
+                    stress = int(stress)
 
-                # Return the prediction as JSON
-                return {"prediction": int(prediction[0])}
+                    # Add your model loading and prediction logic here
+                    # For demonstration purposes, we'll use a placeholder model
+                    # Replace the following lines with your actual model loading and prediction code
+                    model = pickle.load(open('model.pkl', 'rb'))
+                    features = [[1, steps, heart_rate, sleep_duration, stress]]
+                    prediction = model.predict(features)
 
-            except Exception as e:
-                # Handle exceptions, such as invalid input
-                return {"error": str(e)}, 400  # Return a 400 status code for bad requests
-        else:
-            # No specific path parameters provided
-            return {"message": "Please provide values for Steps, HeartRate, SleepDuration, and Stress in the URL."}, 400
+                    # Return the prediction as JSON
+                    return {"prediction": int(prediction[0])}
 
-api.add_resource(MentalHealthPrediction, '/predict', '/predict/<int:steps>/<int:heart_rate>/<float:sleep_duration>/<int:stress>')
+                except Exception as e:
+                    # Handle exceptions, such as invalid input
+                    return {"error": str(e)}, 400  # Return a 400 status code for bad requests
+            else:
+                # No specific path parameters provided
+                return {"message": "Please provide values for Steps, HeartRate, SleepDuration, and Stress in the URL."}, 400
+
+    api.add_resource(MentalHealthPrediction, '/predict', '/predict/<int:steps>/<int:heart_rate>/<float:sleep_duration>/<int:stress>')
+
+    return app
 
 if __name__ == '__main__':
-    # Run the app on a specific port (e.g., 5000)
+    app = create_app()
     app.run(host='0.0.0.0', port=5000)
